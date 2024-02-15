442fc9d78250308782151a09388f8652f9a6e6ab
PranjalPal75389 <palpranjal75389@gmail.com>
2024-02-15 20:19:57 +0530

    g

--- To Do List/script.js
+++ To Do List/script.js
@@ -1,63 +1,50 @@
-const inputField =document.getElementById("inputField");
-const listContainer=document.getElementById("listContainer")
+const inputField = document.getElementById("inputField");
+const listContainer = document.getElementById("listContainer");
 
+function addTask() {
+  if (inputField.value === "") return alert("You must Write Some task first");
+  else {
+    let li = document.createElement("li");
+    li.innerHTML = inputField.value;
+    listContainer.appendChild(li);
 
-function addTask(){
-    if(inputField.value ==="")
-        return alert("You must Write Some task first");
-    
-    else{
-        let li=document.createElement("li");
-        li.innerHTML=inputField.value;
-        listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "&#10006";
+    li.appendChild(span);
+    inputField.value = "";
+  }
+  saveData();
+}
 
-        let span=document.createElement("span");
-        span.innerHTML="&#10006";
-        li.appendChild(span);
-        inputField.value="";
-
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      toggleImage(e.target);
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
     }
-    saveData();
+  },
+  false
+);
+
+function toggleImage(li) {
+  const image = li.querySelector("span");
+  if (li.classList.contains("checked")) {
+    image.style.backgroundImage = 'url("/images/check.webp")';
+  } else {
+    image.style.backgroundImage = 'url("/images/uncheck.webp")';
+  }
+  saveData();
 }
 
-
-listContainer.addEventListener("click", function(e) {
-        if (e.target.tagName === "LI") {
-            e.target.classList.toggle("checked");
-            toggleImage(e.target);
-            saveData(); 
-        } else if (e.target.tagName === "SPAN") {
-            e.target.parentElement.remove();
-             saveData();
-        }
-           
-    },false);
-
-   function toggleImage(li) {
-    const image = li.querySelector("span");
-    if (li.classList.contains("checked")) {
-        image.style.backgroundImage = 'url("/images/check.webp")';
-    } else {
-        image.style.backgroundImage = 'url("/images/uncheck.webp")';
-    }
-    saveData();
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
 }
-
-// listContainer.addEventListener("click",function(e){
-//     if(e.target.tagName ==="LI"){
-//         e.target.classlist.toggle("checked");
-//     }
-//     else if(e.target.tagName==="SPAN"){
-//     e.target.parentElement.remove();
-//     saveData();
-//     }
-// },false);
-
-
-function saveData(){
-    localStorage.setItem("data",listContainer.innerHTML);
-}
-function getData(){
-    listContainer.innerHTML= localStorage.getItem("data");
+function getData() {
+  listContainer.innerHTML = localStorage.getItem("data");
 }
 getData();
