83ad6c2eeb54b38b43b9b9ebd5bfe7686bd6ba8c
PranjalPal75389 <palpranjal75389@gmail.com>
2024-02-22 18:44:56 +0530

    script updated

--- weather/script.js
+++ weather/script.js
@@ -1,3 +1,22 @@
-function searchCity() {
-    console.log("Fuck You");
+const inputCityName = document.getElementById("inputCityName");
+const search = document.getElementById("search-img")
+
+search.addEventListener("click", searchWeather(inputCityName));
+
+function searchWeather() {
+    city_name = inputCityName.value;
+    checkWeather();
+    async function checkWeather() {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&units=metric&appid=0a201b3507ffd904a0ee589f4d533a2c`);
+        var data = await response.json();
+        console.log(data);
+        document.querySelector(".cityName").innerHTML = data.name;
+        document.getElementById("temp").innerHTML = data.main.temp + "°C";
+        document.getElementById("humidity").innerText = data.main.humidity+"%";
+        document.getElementById("windSpeed").textContent = data.wind.speed + "Km/h";
+    }
+
+
+    // document.querySelector("img").src = ""
+
 }
