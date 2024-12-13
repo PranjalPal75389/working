4f380eb96c03db198920dbe45dc7f4bec22ae51f
Pranjal Pal <palpranjal75389@gmail.com>
2024-12-13 11:39:58 +0530

    start typing if any key is pressedf

--- typing/script.js
+++ typing/script.js
@@ -1,23 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   let startTime;
   let quote = "";
-  const submitBtn = document.getElementById("submit-button")
+  const submitBtn = document.getElementById("submit-button");
   const textBox = document.querySelector(".text-box");
-  const textInput = document.querySelector(".text-input")
+  const textInput = document.querySelector(".text-input");
   const lengthBtns = document.querySelectorAll(".length-button");
-  const speakBtn = document.getElementById("speak-button")
+  const speakBtn = document.getElementById("speak-button");
 
-
-  async function fetchText( minLengthValue=100) {
+  textInput.value = "";
+  async function fetchText(minLengthValue = 100) {
     try {
-      const response = await fetch(`https://api.quotable.io/random?minLength=${minLengthValue}`);
+      const response = await fetch(
+        `https://api.quotable.io/random?minLength=${minLengthValue}`
+      );
       const data = await response.json();
       console.log(data);
       quote = data.content;
       console.log(quote);
       console.log(quote.length);
       textBox.textContent = quote;
-      textSpeech();
+      textSpeech(textBox.textContent);
     } catch (error) {
       console.error("Error fetching quote,", error);
       textBox.textContent = "failed to load quote";
@@ -27,10 +29,13 @@ document.addEventListener("DOMContentLoaded", () => {
   // fetch the text using api
   fetchText();
 
-  textInput.onclick = () => {
-    startTime = new Date();
-    console.log("start time = " + startTime);
-  };
+  document.addEventListener("keydown", (e) => {
+    textInput.focus();
+    if (!startTime) {
+      startTime = new Date();
+      console.log("start time = " + startTime);
+    }
+  });
 
   function submitText() {
     if (startTime && quote) {
@@ -45,12 +50,14 @@ document.addEventListener("DOMContentLoaded", () => {
       for (let i = 0; i < quoteWords.length; i++) {
         if (quoteWords[i] === userWords[i]) {
           wordCheck += 1;
-          coloredText=coloredText+` <span style="color:green">${quoteWords[i]}</span> `;
+          coloredText =
+            coloredText + ` <span style="color:green">${quoteWords[i]}</span> `;
         } else {
-          coloredText=coloredText+` <span style="color:red">${quoteWords[i]}</span> `;
+          coloredText =
+            coloredText + ` <span style="color:red">${quoteWords[i]}</span> `;
         }
       }
-      textBox.innerHTML=coloredText.trim();
+      textBox.innerHTML = coloredText.trim();
       let timeTaken = (submitTime - startTime) / 1000;
       let WPM = Math.round((wordCheck / timeTaken) * 60);
       console.log("WPM :" + WPM);
@@ -65,6 +72,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // submit the text on pressing Enter
   document.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
+      responsiveVoice.pause();
       submitText();
     }
   });
@@ -80,37 +88,50 @@ document.addEventListener("DOMContentLoaded", () => {
     e.preventDefault();
     alert("copying are not allowed");
   });
- 
- lengthBtns.forEach(btn => {
+
+  lengthBtns.forEach((btn) => {
     btn.addEventListener("click", async () => {
+      if (responsiveVoice.isPlaying()) {
+        responsiveVoice.cancel();
+      }
       let minLengthValue = parseInt(btn.textContent, 10);
       await fetchText(minLengthValue);
     });
   });
-async function textSpeech(){
-   if(window.speechSynthesis){
-  let speech = new SpeechSynthesisUtterance();
-  speech.text=textBox.textContent;
-    speech.rate=1;
-    speech.pitch=1;
-    speech.onstart = () => {
-      console.log("Speech started");
-    };
-    speech.onend = () => {
-      console.log("Speech ended");
-    };
-    speech.onerror = (event) => {
-      console.error("Speech error: ", event);
-    };
-    speechSynthesis.speak(speech);
+  async function textSpeech(text) {
+    //   let speechText;
+    //    if(window.speechSynthesis){
+    //   let speech = new SpeechSynthesisUtterance();
+    //   speech.text=textBox;
+    //   speechText=textBox;
+    //     speech.rate=1;
+    //     speech.pitch=1;
+    //     speech.onstart = () => {
+    //       console.log("Speech started");
+    //     };
+    //     speech.onend = () => {
+    //       console.log("Speech ended");
+    //     };
+    //     speech.onerror = (event) => {
+    //       console.error("Speech error: ", event);
+    //     };
+    //     speechSynthesis.speak(speech);
 
- } 
- else{
-  if(responsiveVoice.voiceSupport()){
-    responsiveVoice.speak(textBox.textContent);
+    //  }
+    //  else{
+    if (responsiveVoice.voiceSupport()) {
+      let speechText;
+      speechText = text;
+      responsiveVoice.speak(speechText);
+    }
+    //  }
   }
- }
-}
-// text to speak button event listener 
-speakBtn.addEventListener("click",textSpeech);
+  // text to speak button event listener
+  speakBtn.addEventListener("click", () => {
+    if (responsiveVoice.isPlaying()) {
+      responsiveVoice.pause();
+    } else {
+      responsiveVoice.resume();
+    }
+  });
 });
