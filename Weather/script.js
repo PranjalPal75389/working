834decc3a7eb8a0986b197d6986a17faadf591bc
Pranjal Pal <palpranjal75389@gmail.com>
2024-12-26 14:26:42 +0530

    Enter to fetch data

--- Weather/script.js
+++ Weather/script.js
@@ -1,23 +1,23 @@
-
-search.addEventListener("click", searchWeather(inputCityName));
-
-function searchWeather() {
     const inputCityName = document.getElementById("inputCityName");
     const search = document.getElementById("search-img")
 
-    city_name = inputCityName.value;
+function searchWeather() {
+    let city_name = inputCityName.value ? inputCityName.value : "mumbai";
     checkWeather();
     async function checkWeather() {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&units=metric&appid=0a201b3507ffd904a0ee589f4d533a2c`);
         var data = await response.json();
         console.log(data);
-        document.querySelector(".cityName").innerHTML = data.name;
-        document.getElementById("temp").innerHTML = data.main.temp + "°C";
-        document.getElementById("humidity").innerText = data.main.humidity+"%";
-        document.getElementById("windSpeed").textContent = data.wind.speed + "Km/h";
+        document.querySelector(".cityName").innerHTML = data.name ? data.name : "No city found";
+        document.getElementById("temp").innerHTML = data.main  ? data.main.temp + "°C" : "-°C";
+        document.getElementById("humidity").innerText = data.main ? data.main.humidity + "%" : "-%";
+        document.getElementById("windSpeed").textContent = data.wind  ? data.wind.speed + " Km/h" : "- Km/h";
+        inputCityName.value ="";
     }
-
-
-    // document.querySelector("img").src = ""
-
 }
+search.addEventListener("click", searchWeather(inputCityName));
+window.document.addEventListener("keydown",(e)=>{
+    if(e.key==="Enter"){
+        searchWeather(inputCityName);
+    }
+})
